44fb4ef1f11e14034c21296c7b0b1d41f803584f
Madison Kimble <mkkimble@live.unc.edu>
2022-01-21 19:35:04 -0500

    actually finished ex1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,10 +4,12 @@ __author__ = "730244272"
 
 five_character_word: str = input("Enter a 5-character word: ")
 if len(five_character_word) != 5:
-    exit("Error: Word must contain 5 characters.")
+    print("Error: Word must contain 5 characters")
+    exit()
 single_character: str = input("Enter a single character: ")
 if len(single_character) != 1:
-    exit("Error: Character must be a single character.")
+    print("Error: Character must be a single character.")
+    exit()
 
 print("Searching for " + single_character + " in " + five_character_word)
 how_many: int = five_character_word.count(single_character)
