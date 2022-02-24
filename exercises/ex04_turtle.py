9b3cda502bffd5f19aa96aee5b15e09bee12e406
Madison Kimble <mkkimble@live.unc.edu>
2022-02-23 20:08:37 -0500

    last

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -22,6 +22,10 @@ def main() -> None:
 # 3rd window (I was going for attic window, not quite sure if it looks like that.)
     draw_door(piggie, -25, -100)
 # Lines 72-87
+    draw_top_tree(piggie, 200, 20)
+# Lines 94-107
+    draw_bottom_tree(piggie, 230, -100)
+# Lines 112-126
     done()
 
 
@@ -56,7 +60,6 @@ def draw_roof(second: Turtle, z: float, w: float) -> None:
     piggie.right(135)
     piggie.forward(142)
     piggie.end_fill()
-    piggie.penup()
 
 
 def draw_window(three4th: Turtle, a: float, b: float) -> None:
@@ -86,6 +89,39 @@ def draw_door(last: Turtle, c: float, d: float) -> None:
     piggie.forward(50)
     piggie.left(90)
     piggie.forward(100)
+    piggie.end_fill()
+
+
+def draw_top_tree(f: float, g: float) -> None:
+    """Drawing tree-top, shaded in a darker green than the line color."""
+    piggie.penup()
+    piggie.goto(f, g)
+    piggie.pendown()
+    piggie.begin_fill()
+    piggie.fillcolor(67, 166, 65)
+    piggie.pencolor(18, 192, 14)
+    i: int = 0
+    while i < 3:
+        piggie.forward(100)
+        piggie.left(120)
+        i += 1 
+    piggie.end_fill()
+
+
+def draw_bottom_tree(h: float, i: float) -> None:
+    """Drawing trunk of tree in brown."""
+    piggie.penup()
+    piggie.goto(h, i)
+    piggie.pendown()
+    piggie.begin_fill()
+    piggie.color(130, 75, 55)
+    piggie.forward(30)
+    piggie.left(90)
+    piggie.forward(120)
+    piggie.left(90)
+    piggie.forward(30)
+    piggie.left(90)
+    piggie.forward(120)
     piggie.end_fill()
 
 
